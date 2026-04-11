70e913e08e02c90c3a52ec024801920f95890ddf
RHAINE <yudai.bernard@gmail.com>
2026-04-11 20:19:12 +0800

    fixed central domain login error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,14 +3,15 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         using: function () {
-            require __DIR__.'/../routes/web.php';
-            require __DIR__.'/../routes/tenant.php';
+            Route::middleware('web')->group(base_path('routes/web.php'));
+            Route::middleware('web')->group(base_path('routes/tenant.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
