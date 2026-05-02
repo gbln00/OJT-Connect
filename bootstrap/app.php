b82f83f3d81f8b8401528a20e2c10b4b8d233d28
RHAINE <yudai.bernard@gmail.com>
2026-05-02 15:13:41 +0800

    fix: trust Railway proxy for HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
 
         $middleware->redirectGuestsTo('/login');
+        $middleware->trustProxies(at: '*');
 
         $middleware->validateCsrfTokens(except: [
             'login',
