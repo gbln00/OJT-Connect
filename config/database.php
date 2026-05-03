2a13211edfc252bdeb621980d6896e37d241755f
RHAINE <yudai.bernard@gmail.com>
2026-05-03 21:26:14 +0800

    fix: add central database connection for Stancl Tenancy

--- config/database.php
+++ config/database.php
@@ -63,6 +63,22 @@ return [
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ] : [],
         ],
+        'central' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', '127.0.0.1'),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => env('DB_DATABASE', 'laravel'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix'    => '',
+            'strict'    => true,
+            'engine'    => null,
+            'options'   => env('DB_SSL') ? [
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ] : [],
+        ],
 
         'mariadb' => [
             'driver' => 'mariadb',
