1bbe1e5a27326945108f672c366247e84144ae8b
RHAINE <yudai.bernard@gmail.com>
2026-05-04 14:11:11 +0800

    fixed connection in session

--- config/session.php
+++ config/session.php
@@ -73,7 +73,7 @@ return [
     |
     */
 
-    'connection' => env('SESSION_CONNECTION', 'central'),
+    'connection' => env('SESSION_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
