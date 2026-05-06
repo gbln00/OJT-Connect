f6485f690cbf390b5a62b1463877566d9805e3c9
RHAINE <yudai.bernard@gmail.com>
2026-05-07 07:25:18 +0800

    TEST: Announcement  Announcement  Announcement  Announcement

---
+++ database/factories/AnnouncementFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Announcement;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Announcement>
+ */
+class AnnouncementFactory extends Factory
+{
+    protected $model = Announcement::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(4),
+            'body' => $this->faker->paragraph(3),
+        ];
+    }
+}
